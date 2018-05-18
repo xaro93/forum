d083896d6cc7f3ed1a830b153919927f944d9766
Paweł Wójcik <pwojcik.main@gmail.com>
2018-05-18 21:38:12 +0000

    Config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,12 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
+
+    .addEntry('forum', './assets/forum/js/app.js')
     .autoProvidejQuery()
-    .autoProvideVariables({
-        "window.Bloodhound": require.resolve('bloodhound-js'),
-        "jQuery.tagsinput": "bootstrap-tagsinput"
-    })
-    .enableSassLoader()
-    .enableVersioning(false)
-    .createSharedEntry('js/common', ['jquery'])
-    .addEntry('js/app', './assets/js/app.js')
-    .addEntry('js/login', './assets/js/login.js')
-    .addEntry('js/admin', './assets/js/admin.js')
-    .addEntry('js/search', './assets/js/search.js')
-    .addStyleEntry('css/app', ['./assets/scss/app.scss'])
-    .addStyleEntry('css/admin', ['./assets/scss/admin.scss'])
+    .enableSourceMaps(!Encore.isProduction())
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
 ;
 
 module.exports = Encore.getWebpackConfig();
