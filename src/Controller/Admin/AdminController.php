635e3c3e3875dafa3257e10cdca70b9e983f6a2b
Paweł Wójcik <pwojcik.main@gmail.com>
2018-05-23 21:04:21 +0000

    new concept

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xaro
+ * Date: 31.03.2018
+ * Time: 11:12
+ */
+
+namespace App\Controller\Admin;
+
+
+use App\Controller\BaseController;
+
+class AdminController extends BaseController
+{
+    const FORM_PATH = 'App\Form\Admin\\';
+}
---
+++ src/Controller/Forum/ForumController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xaro
+ * Date: 31.03.2018
+ * Time: 11:12
+ */
+
+namespace App\Controller\Forum;
+
+
+use App\Controller\BaseController;
+
+use App\Entity\Thread;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ForumController extends BaseController
+{
+    public $formPath = __NAMESPACE__;
+
+}
